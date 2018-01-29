afd2132b64339828eb3d56bf09632b0147d91048
Ryan Barlow <ryanbarlow2@mail.weber.edu>
2018-01-29 08:09:26 -0700

    Initial commit for Selenium_Search

---
+++ Selenium_Search/Selenium_Search/Program.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Search
+{
+    class Program
+    {
+        private IWebElement query;
+
+        public void Web_Search_Test()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(https://www.google.com/)";
+
+            IWebElement query = driver.FindElement(By.Id("lst-ib"));
+            query.SendKeys("selenium hq");
+
+            driver.Close();
+        }
+    }
+}
