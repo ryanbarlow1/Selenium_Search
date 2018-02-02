3f406bffa389bd5a8844f4a1fdc3628d8fb153e8
Ryan Barlow <ryanbarlow2@mail.weber.edu>
2018-02-02 00:50:25 -0700

    Fixed compiling error in initial commit

---
+++ SeleniumSearch/SeleniumSearch/Program.cs
@@ -0,0 +1,25 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenium_Search
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("http://www.google.com");
+            IWebElement element = driver.FindElement(By.Name("lst-ib"));
+            element.SendKeys("Selenium HQ");
+
+
+            driver.Close();
+        }
+    }
+}
